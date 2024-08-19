5ac7a55bb4c33592388d92adb891499a9106db69
MarkoCutura56 <107407863+MarkoCutura56@users.noreply.github.com>
2024-08-19 21:21:44 +0200

    Update views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -113,9 +113,9 @@ def add_review(request):
     if (request.user.is_anonymous == False): # noqa
         data = json.loads(request.body)
         try:
-            response = post_review(data)
+            response = post_review(data)# noqa
             return JsonResponse({"status": 200})
         except:# noqa
             return JsonResponse({"status": 401,"message": "Error in posting review"})# noqa
     else:
-        return JsonResponse({"status": 403,"message": "Unauthorized"})
+        return JsonResponse({"status": 403, "message": "Unauthorized"})
