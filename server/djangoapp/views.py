48071f436842d0cb50f0264bf95aea4fed7d937f
MarkoCutura56 <107407863+MarkoCutura56@users.noreply.github.com>
2024-08-19 21:14:00 +0200

    Update views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -58,6 +58,8 @@ def registration(request):
     else :
         data = {"userName":username,"error": "Already Registered"}
         return JsonResponse(data)
+
+
 # @csrf_exempt # noqa
 def get_dealerships(request, state="All"):
     if(state == "All"):
@@ -67,6 +69,7 @@ def get_dealerships(request, state="All"):
     dealerships = get_request(endpoint)
     return JsonResponse({"status": 200,"dealers": dealerships})
 
+
 def get_dealer_details(request, dealer_id):
     if(dealer_id):
         endpoint = "/fetchDealer/"+str(dealer_id)
@@ -74,6 +77,7 @@ def get_dealer_details(request, dealer_id):
         return JsonResponse({"status": 200,"dealer": dealership})
     else:
         return JsonResponse({"status": 400,"message": "Bad Request"})
+
 
 def get_dealer_reviews(request, dealer_id):
     # if dealer id has been provided
@@ -88,6 +92,7 @@ def get_dealer_reviews(request, dealer_id):
     else:
         return JsonResponse({"status": 400,"message": "Bad Request"})
 
+
 def get_cars(request):
     count = CarMake.objects.filter().count()
     print(count)
@@ -98,6 +103,7 @@ def get_cars(request):
     for car_model in car_models:
         cars.append({"CarModel": car_model.name, "CarMake": car_model.car_make.name})
     return JsonResponse({"CarModels":cars})
+
 
 def add_review(request):
     if(request.user.is_anonymous == False):
