7a6e274432ea4a2ed3b69ad49c8c81f82b9a73d3
MarkoCutura56 <107407863+MarkoCutura56@users.noreply.github.com>
2024-08-19 20:28:19 +0200

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -3,16 +3,16 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 class CarMake(models.Model):
-    
+
     name = models.CharField(max_length=100)
     description = models.TextField()
     # Other fields as needed
 
     def __str__(self):
         return self.name  # Return the name as the string representation
-        
+   
 class CarModel(models.Model):
-    
+
     car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     CAR_TYPES = [
@@ -26,6 +26,6 @@ class CarModel(models.Model):
             MaxValueValidator(2023),
             MinValueValidator(2015)
         ])
-    
+
     def __str__(self):
         return self.name  # Return the name as the string representation
